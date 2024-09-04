f73c8b355300bff202c40a80e285fe712e8590da
mreduzino <marceloreduzino@hotmail.com>
2024-09-04 11:26:34 -0300

    Iniciando projeto em Java da aula Tecnicas de Programação I

---
+++ semestre2/Tec.Prog.I/Java/Livro/Livro/src/Livro.java
@@ -0,0 +1,18 @@
+public class Livro {
+    
+    String nome;
+    String descricao;
+    double valor;
+    String isbn;
+    Autor autor;
+
+    void mostrarDetalhes() {
+        System.out.println("Mostrando detalhes do livro");
+        System.out.println("Nome: " + nome);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Valor: " + valor);
+        System.out.println("ISBN: " + isbn);
+        autor.mostrarDetalhes();
+        System.out.println("--");
+        }
+    }
